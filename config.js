6404bb5e7456b8035b292f21a39eb21da48821e4
Kagiso Nyokolodi <knyokolodi@gmail.com>
2019-04-08 09:37:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,6 +18,23 @@ if (process.env.NODE_ENV == 'development'){
     config.mail.sendTo = 'knyokolodi@gmail.com';
     config.mail.port = 465;
 
+}if(process.env.NODE_ENV == 'production'){
+    config.mongo = {};
+    config.mongo.database = '';
+    config.mongo.password = 'wicode2019'
+
+    config.api = {};
+    config.api.url = 'https://wicode-api.herokuapp.com/';
+
+    config.jwt = {};
+    config.jwt.key = 'secret';
+
+    config.mail = {}
+    config.mail.username = 'knyokolodi@gmail.com';
+    config.mail.password = 'Keos@89072018';
+    config.mail.service = 'Gmail';
+    config.mail.sendTo = 'knyokolodi@gmail.com';
+    config.mail.port = 465;
 }else{
     process.exit(console.log('No environment variable specified. Killing app now.'));
 }
