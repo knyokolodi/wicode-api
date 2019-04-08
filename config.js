808d5ac7a6dc7f476a2dee7ab9ca269898897ff0
Kagiso Nyokolodi <knyokolodi@gmail.com>
2019-04-08 10:04:47 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,24 +1,4 @@
 const config = {}
-
-if (process.env.NODE_ENV == 'development'){
-    config.mongo = {};
-    config.mongo.database = '';
-    config.mongo.password = 'wicode2019'
-
-    config.api = {};
-    config.api.url = 'http://localhost:3000/';
-
-    config.jwt = {};
-    config.jwt.key = 'secret';
-
-    config.mail = {}
-    config.mail.username = 'knyokolodi@gmail.com';
-    config.mail.password = 'Keos@89072018';
-    config.mail.service = 'Gmail';
-    config.mail.sendTo = 'knyokolodi@gmail.com';
-    config.mail.port = 465;
-
-}if(process.env.NODE_ENV == 'production'){
     config.mongo = {};
     config.mongo.database = '';
     config.mongo.password = 'wicode2019'
@@ -35,8 +15,4 @@ if (process.env.NODE_ENV == 'development'){
     config.mail.service = 'Gmail';
     config.mail.sendTo = 'knyokolodi@gmail.com';
     config.mail.port = 465;
-}else{
-    process.exit(console.log('No environment variable specified. Killing app now.'));
-}
-
 module.exports = config;
